56f38782f832c7f84e88404d115beb5bfc044bfa
1-Koval4uk <46053952+1-Koval4uk@users.noreply.github.com>
2020-04-18 13:30:08 +0300

    Finished. without adaptive

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.load-more').click(function() {
+        $('.hiden').addClass('portfolio-wrap__cards').removeClass('hiden')
+    })
+
+
+
+    $('.testimonials-wrap__slider').slick();
+})
+
+
